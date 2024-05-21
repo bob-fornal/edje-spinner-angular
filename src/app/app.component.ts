20719d9fd12af2d2b0eef69de3e6c35855be3508
Bob Fornal <bob.fornal@leadingedje.com>
2024-05-21 13:47:59 -0400

    Basic Functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +6,15 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
-  title = 'fe-angular';
+  active: boolean = false;
+  config: { [key: string]: string } = {
+    display: 'round',
+    type: 'le',
+  };
+
+  stringifyConfig = (): string => JSON.stringify(this.config);
 }
